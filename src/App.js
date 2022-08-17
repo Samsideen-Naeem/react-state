e9b1720a713778c6ad4a47513453d613544af729
Samsideen-Naeem <110115318+Samsideen-Naeem@users.noreply.github.com>
2022-08-17 18:21:57 +0000

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import NavBar from './components/NavBar'
-import Todo from './components/Todo'
-
+import ProfileCard from './components/ProfileForm'
+import ProfileForm from './components/ProfileForm'
 function App() {
-
-let title ="My activities to do for the weak"
   return (
     <div>
-      <NavBar title={title} />
-      <main>
-      <Todo title="learning CSS" datetime="15 August2022" /> 
-      <Todo title="learning HTML" datetime="15 August2022" /> 
-      <Todo title="learning Javascript" datetime="15 August2022" /> 
-      <Todo title="learning Web browser" datetime="15 August2022" /> 
-      <Todo title="learning react" datetime="15 August2022" /> 
-            
-
- </main>     
+      
     </div>
   )
 }
